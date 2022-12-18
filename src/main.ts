214c36e76de87cda501a1e80ff2cd08bce96fa3b
giuseppegrilli <giugrilli@gmail.com>
2022-12-19 00:23:07 +0100

    dirty audio speech to text

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { env } from './utils/env';
 import { create, resetLogin, send, isLogged } from './conversation';
 import { editMessage } from './bot';
 import { UNLOCK_THOUGHT_CONTROL_MESSAGE } from './constants/message';
+import fs from 'fs'
+import sdk from 'microsoft-cognitiveservices-speech-sdk'
+import axios from 'axios'
+import stream from 'stream';
+import util from 'util'
+import ffmpeg from 'fluent-ffmpeg'
 
+const pipeline = util.promisify(stream.pipeline);
 const allowed_ids = env.ALLOWED_IDS.split(',')
 
 // Create a new telegraf bot instance
@@ -17,7 +24,7 @@ bot.start(async (ctx) => {
   console.log('start', ctx.from);
   const id = ctx.from?.id;
 
-  if (!allowed_ids.includes(id.toString())){
+  if (!allowed_ids.includes(id.toString())) {
     return ctx.reply('❌ Not Allowed ❌');
   }
 
@@ -37,17 +44,15 @@ bot.start(async (ctx) => {
   return ctx.reply(`Hello ${ctx.from?.first_name}! Let's chat`, keyboard);
 });
 
-const checkPreviousContext = (message:any) => {
+const checkPreviousContext = (message: any) => {
   const previousMessage = message.reply_to_message?.text
   let contextArray = []
-  if (previousMessage){
-    // @ts-ignore
+  if (previousMessage) {
     const regex = /\{GPTContext:\[.*\]\}/
-    // @ts-ignore
     const hasPreviousContext = regex.test(previousMessage)
-    if(hasPreviousContext){
+    if (hasPreviousContext) {
       contextArray = previousMessage.substring(
-        previousMessage.indexOf("[") + 1, 
+        previousMessage.indexOf("[") + 1,
         previousMessage.lastIndexOf("]")
       ).split(',')
     }
@@ -55,23 +60,118 @@ const checkPreviousContext = (message:any) => {
   return contextArray
 }
 
+// This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+
+
+
+const convertVoiceToText = async (ctx: any) => {
+
+  const speechConfig = sdk.SpeechConfig.fromSubscription(env.AZURE_SPEECH_KEY, env.AZURE_SPEECH_REGION);
+  speechConfig.speechRecognitionLanguage = "it-IT";
+
+  const audioUrl = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+  console.log("AUDIOURL", audioUrl)
+
+  const response = await axios({
+    method: 'GET',
+    url: audioUrl.href,
+    responseType: 'stream'
+  });
+
+  const filename = Buffer.from(Math.random().toString()).toString("base64").substring(10, 15);
+  await pipeline(response.data, fs.createWriteStream(`tmp/${filename}.oga`));
+
+  await new Promise((resolve, reject) => {
+    ffmpeg(`tmp/${filename}.oga`)
+      .output(`tmp/${filename}.wav`)
+      .on('end', () => resolve(1))
+      .run();
+  })
+  const wavFile = fs.readFileSync(`tmp/${filename}.wav`);
+  const audioConfig = sdk.AudioConfig.fromWavFileInput(wavFile);
+  const speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+  const finalText = await new Promise((resolve, reject) => {
+    speechRecognizer.recognizeOnceAsync(
+      (result: any) => {
+        switch (result.reason) {
+          case sdk.ResultReason.RecognizedSpeech:
+            console.log(`RECOGNIZED: Text=${result.text}`);
+            resolve(result.text)
+            break;
+          case sdk.ResultReason.NoMatch:
+            console.log("NOMATCH: Speech could not be recognized.");
+            reject()
+            break;
+          case sdk.ResultReason.Canceled:
+            const cancellation = sdk.CancellationDetails.fromResult(result);
+            console.log(`CANCELED: Reason=${cancellation.reason}`);
+            if (cancellation.reason == sdk.CancellationReason.Error) {
+              console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+              console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+              console.log("CANCELED: Did you set the speech resource key and region values?");
+            }
+            reject()
+            break;
+        }
+        speechRecognizer.close();
+      })
+  })
+
+
+  fs.unlink(`tmp/${filename}.oga`, function (err) {
+    if (err) {
+      console.error(err);
+      console.log('File not found');
+    } else {
+      console.log('File Delete Successfuly');
+    }
+  });
+  fs.unlink(`tmp/${filename}.wav`, function (err) {
+    if (err) {
+      console.error(err);
+      console.log('File not found');
+    } else {
+      console.log('File Delete Successfuly');
+    }
+  });
+
+  return String(finalText)
+}
+
 // When the bot receives a text message
-bot.on('text', async (ctx) => {
-  // Get the text of the message and the user's ID
-  const text = ctx.message?.text.trim();
+bot.on('message', async (ctx) => {
   const id = ctx.from?.id;
 
-  if (!allowed_ids.includes(id.toString())){
+  if (!allowed_ids.includes(id.toString())) {
     await ctx.reply('❌ Not Allowed ❌')
-    return 
+    return
   }
+
+  let text = ''
+
+  // @ts-ignore
+  if (ctx.message.text) {
+    // @ts-ignore
+    text = ctx.message?.text.trim();
+    // @ts-ignore
+  } else if (ctx.message.voice) {
+    text = await convertVoiceToText(ctx)
+    ctx.sendMessage(`ConvertedText: ${text}`);
+  } else {
+    return
+  }
+
+  if (!text || text === '') {
+    return
+  }
+
   let contextArray = checkPreviousContext(ctx.message)
 
   const getRandomGif = await fetch(
     "http://api.giphy.com/v1/gifs/random?tag=thinking&api_key=Al5wJ2bX1FQPosMW1BCgTNlho1j37MB8"
-  );  
+  );
 
-  const removeMessages = async (message:any, animationmessage:any, loginMessage:any) => {
+  const removeMessages = async (message: any, animationmessage: any, loginMessage: any) => {
     await Promise.all([
       message && ctx.telegram.deleteMessage(message.chat.id, message.message_id),
       animationmessage && ctx.telegram.deleteMessage(animationmessage.chat.id, animationmessage.message_id),
@@ -110,20 +210,21 @@ bot.on('text', async (ctx) => {
         const response = await send(id, text, contextArray);
         const messages = {
           sender: ctx.from,
-          query: text, 
+          query: text,
           response: response
         }
         console.log('messages', messages);
         // delete the message and send a new one to notice the user
         await removeMessages(message, animationmessage, loginMessage)
-        const finalResponse = response.response+`\n\n {GPTContext:[${response.conversationId},${response.messageId}]}`
+        const finalResponse = response.response + `\n\n {GPTContext:[${response.conversationId},${response.messageId}]}`
         await ctx.reply(finalResponse, removeKeyboard)
-      } catch (e: any) { 
+      } catch (e: any) {
         await removeMessages(message, animationmessage, loginMessage)
         await ctx.sendMessage('❌ Something went wrong. Details: ' + e.message, removeKeyboard)
         await ctx.sendMessage('❗️ Just try again now ❗️');
       }
   }
+
 });
 
 bot.catch(console.error);
